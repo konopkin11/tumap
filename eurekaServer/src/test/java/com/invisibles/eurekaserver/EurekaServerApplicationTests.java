adb053777f280403de63d60c05abf820767f7c07
evgenijkonopkin <konopkin11@gmail.com>
2023-05-08 15:37:02 +0300

    added eureka

---
+++ eurekaServer/src/test/java/com/invisibles/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invisibles.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- schedule-service/src/main/java/com/invisibles/scheduleservice/ScheduleServiceApplication.java
+++ schedule-service/src/main/java/com/invisibles/scheduleservice/ScheduleServiceApplication.java
@@ -2,12 +2,13 @@ package com.invisibles.scheduleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableScheduling
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class ScheduleServiceApplication {
 
     public static void main(String[] args) {
